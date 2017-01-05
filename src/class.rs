9512ab3814dbdd788bdc15ea3fe45798abc9bdbe
Benjamin Cheng <ben@bcheng.cf>
2017-01-05 14:40:40 -0500

    Split-up files; Read fields

---
+++ src/class.rs
@@ -0,0 +1,68 @@
+use std::io::{Cursor, Read};
+
+use super::constant_info::ConstantInfo;
+
+use byteorder::{BigEndian, ReadBytesExt};
+
+const MAGIC_VALUE: u32 = 0xCAFEBABE;
+
+#[derive(Debug)]
+pub struct Class {
+    minor_version: u16,
+    major_version: u16,
+    constant_pool_len: u16,
+    constant_pool: Box<[ConstantInfo]>,
+    access_flags: u16,
+    this_class: u16,
+    super_class: u16,
+    interfaces_len: u16,
+    interfaces: Box<[u16]>,
+    /*
+    fields_len: u16,
+    fields: Box<[FieldInfo]>,
+    methods_count: u16,
+    methods: Box<[MethodInfo]>,
+    attributes_count: u16,
+    attributes: Box<[AttributeInfo]>
+    */
+}
+
+impl Class {
+    pub fn new(data: Vec<u8>) -> Class {
+        let mut cur = Cursor::new(data);
+        assert_eq!(MAGIC_VALUE, cur.read_u32::<BigEndian>().unwrap());
+        
+        let minor_version = cur.read_u16::<BigEndian>().unwrap();
+        let major_version = cur.read_u16::<BigEndian>().unwrap();
+        
+        let constant_pool_len = cur.read_u16::<BigEndian>().unwrap();
+        let mut constant_pool = Vec::with_capacity((constant_pool_len - 1) as usize);
+        for i in 1..constant_pool_len {
+            let tag = cur.read_u8().unwrap();
+            constant_pool.push(ConstantInfo::new(tag, &mut cur));
+        }
+
+        let access_flags = cur.read_u16::<BigEndian>().unwrap();
+        let this_class = cur.read_u16::<BigEndian>().unwrap();
+        let super_class = cur.read_u16::<BigEndian>().unwrap();
+
+        let interfaces_len = cur.read_u16::<BigEndian>().unwrap();
+        let mut interfaces = Vec::with_capacity(interfaces_len as usize);
+        for i in 0..interfaces_len {
+            interfaces.push(cur.read_u16::<BigEndian>().unwrap());
+        }
+        Class {
+            minor_version: minor_version,
+            major_version: major_version,
+            constant_pool_len: constant_pool_len,
+            constant_pool: constant_pool.into_boxed_slice(),
+            access_flags: access_flags,
+            this_class: this_class,
+            super_class: super_class,
+            interfaces_len: interfaces_len,
+            interfaces: interfaces.into_boxed_slice()
+        }
+    }
+}
+
+
---
+++ src/constant_info.rs
@@ -0,0 +1,94 @@
+use std::io::{Cursor, Read};
+
+use byteorder::{BigEndian, ReadBytesExt};
+
+#[derive(Debug)]
+pub enum ConstantInfo {
+    Class { name_index: u16 },
+    Fieldref { class_index: u16, name_and_type_index: u16 },
+    Methodref { class_index: u16, name_and_type_index: u16 },
+    InterfaceMethodref { class_index: u16, name_and_type_index: u16 },
+    String { string_index: u16 },
+    Integer { value: i32 },
+    Float { value: f32 },
+    Long { value: i64 },
+    Double { value: f64 },
+    NameAndType { name_index: u16, descriptor_index: u16 },
+    Utf8 { length: u16, value: String }
+}
+
+impl ConstantInfo {
+    pub fn new(tag: u8, cur: &mut Cursor<Vec<u8>>) -> ConstantInfo {
+        match tag {
+            7 => {
+                ConstantInfo::Class {
+                    name_index: cur.read_u16::<BigEndian>().unwrap()
+                }
+            },
+            9 => {
+                ConstantInfo::Fieldref {
+                    class_index: cur.read_u16::<BigEndian>().unwrap(),
+                    name_and_type_index: cur.read_u16::<BigEndian>().unwrap()
+                }
+            },
+            10 => {
+                ConstantInfo::Methodref {
+                    class_index: cur.read_u16::<BigEndian>().unwrap(),
+                    name_and_type_index: cur.read_u16::<BigEndian>().unwrap()
+                }
+            },
+            11 => {
+                ConstantInfo::InterfaceMethodref {
+                    class_index: cur.read_u16::<BigEndian>().unwrap(),
+                    name_and_type_index: cur.read_u16::<BigEndian>().unwrap()
+                }
+            },
+            8 => {
+                ConstantInfo::String {
+                    string_index: cur.read_u16::<BigEndian>().unwrap()
+                }
+            },
+            3 => {
+                ConstantInfo::Integer {
+                    value: cur.read_i32::<BigEndian>().unwrap()
+                }
+            },
+            4 => {
+                ConstantInfo::Float {
+                    value: cur.read_f32::<BigEndian>().unwrap()
+                }
+            },
+            5 => {
+                ConstantInfo::Long {
+                    value: cur.read_i64::<BigEndian>().unwrap()
+                }
+            },
+            6 => {
+                ConstantInfo::Double {
+                    value: cur.read_f64::<BigEndian>().unwrap()
+                }
+            },
+            12 => {
+                ConstantInfo::NameAndType {
+                    name_index: cur.read_u16::<BigEndian>().unwrap(),
+                    descriptor_index: cur.read_u16::<BigEndian>().unwrap()
+                }
+            },
+            1 => {
+                let length = cur.read_u16::<BigEndian>().unwrap(); 
+                let bytes = vec![0 as u8; length as usize];
+                let mut slice = bytes.into_boxed_slice();
+                cur.read_exact(&mut slice);
+                let bytes = slice.into_vec();
+                ConstantInfo::Utf8 {
+                    length: length,
+                    value: String::from_utf8(bytes).unwrap()
+                }
+            },
+            _ => {
+                panic!("Unknown constant type {}", tag);
+            }
+        }
+    }
+}
+
