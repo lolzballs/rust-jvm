412ef7aa40d4881e257646599d8663091df12c30
Benjamin Cheng <ben@bcheng.cf>
2017-01-08 13:16:37 -0500

    Implement a bunch of instructions

--- src/vm/frame.rs
+++ src/vm/frame.rs
@@ -1,6 +1,7 @@
 use super::class::Class;
-use super::Value;
 use super::constant_pool::ConstantPoolEntry;
+use super::opcode;
+use super::Value;
 
 #[derive(Debug)]
 pub struct Frame<'a> {
@@ -33,6 +34,12 @@ impl<'a> Frame<'a> {
     }
 
     pub fn run(mut self) -> Option<Value> {
+        macro_rules! push {
+            ($v: expr) => ({
+                self.operand_stack.push($v);
+            });
+        }
+
         macro_rules! pop {
             () => (self.operand_stack.pop().unwrap());
             ($value_variant: path) => ({
@@ -44,40 +51,62 @@ impl<'a> Frame<'a> {
             });
         }
 
+        macro_rules! store {
+            ($index: expr) => ({
+                let value = pop!();
+                self.local_variables[$index as usize] = Some(value);
+            });
+        }
+
         loop {
             match self.read_u8() {
-                0x05 => {
-                    // iconst_2
-                    self.operand_stack.push(Value::Int(2));
-                }
-                0x3C => {
-                    // istore_1
-                    self.local_variables[1] = self.operand_stack.pop();
-                }
-                0x10 => {
-                    // bipush
+                opcode::NOP => (),
+                // opcode::ACONST_NULL => push!(Value::NullReference),
+                opcode::ICONST_M1 => push!(Value::Int(-1)),
+                opcode::ICONST_0 => push!(Value::Int(0)),
+                opcode::ICONST_1 => push!(Value::Int(1)),
+                opcode::ICONST_2 => push!(Value::Int(2)),
+                opcode::ICONST_3 => push!(Value::Int(3)),
+                opcode::ICONST_4 => push!(Value::Int(4)),
+                opcode::ICONST_5 => push!(Value::Int(5)),
+                opcode::LCONST_0 => push!(Value::Long(0)),
+                opcode::LCONST_1 => push!(Value::Long(1)),
+                opcode::FCONST_0 => push!(Value::Float(0.0)),
+                opcode::FCONST_1 => push!(Value::Float(1.0)),
+                opcode::FCONST_2 => push!(Value::Float(2.0)),
+                opcode::DCONST_0 => push!(Value::Double(0.0)),
+                opcode::DCONST_1 => push!(Value::Double(1.0)),
+                opcode::BIPUSH => {
                     let byte = self.read_u8();
-                    self.operand_stack.push(Value::Int(byte as i32));
+                    push!(Value::Int(byte as i32));
                 }
-                0x1B => {
-                    // iload_1
+                opcode::SIPUSH => {
+                    let short = self.read_u16();
+                    push!(Value::Int(short as i32));
+                }
+                opcode::LDC => {
+                    let index = self.read_u8();
+                    push!(self.class.get_constant_pool().resolve_literal(index as u16).clone());
+                }
+                opcode::ISTORE_0 => store!(0),
+                opcode::ISTORE_1 => store!(1),
+                opcode::ISTORE_2 => store!(2),
+                opcode::ISTORE_3 => store!(3),
+                opcode::ILOAD_1 => {
                     let local = self.local_variables[1].clone().unwrap();
-                    self.operand_stack.push(local);
+                    push!(local);
                 }
-                0x60 => {
-                    // iadd
+                opcode::IADD => {
                     let val2 = pop!(Value::Int);
                     let val1 = pop!(Value::Int);
                     self.operand_stack.push(Value::Int(val1 + val2));
                 }
-                0x64 => {
-                    // isub
+                opcode::ISUB => {
                     let val2 = pop!(Value::Int);
                     let val1 = pop!(Value::Int);
                     self.operand_stack.push(Value::Int(val1 - val2));
                 }
-                0xB8 => {
-                    // invokestatic
+                opcode::INVOKESTATIC => {
                     let index = self.read_u16();
                     if let Some(ConstantPoolEntry::MethodRef(ref symref)) =
                         self.class.get_constant_pool()[index] {
@@ -91,8 +120,7 @@ impl<'a> Frame<'a> {
                         panic!("invokestatic must refer to a MethodRef");
                     }
                 }
-                0xB1 => {
-                    // return
+                opcode::RETURN => {
                     return None;
                 }
                 _ => {
--- src/vm/mod.rs
+++ src/vm/mod.rs
@@ -1,6 +1,8 @@
 pub mod class;
 pub mod constant_pool;
 pub mod frame;
+#[allow(dead_code)]
+pub mod opcode;
 pub mod sig;
 pub mod symref;
 
