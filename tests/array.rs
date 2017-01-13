6c81ef87f5118aa5e92b1e9f757645a0e153add5
Benjamin Cheng <ben@bcheng.cf>
2017-01-12 19:27:35 -0500

    WIP array test

---
+++ tests/array.rs
@@ -0,0 +1,31 @@
+#[cfg(test)]
+
+extern crate rust_jvm;
+
+use std::num::Wrapping;
+use rust_jvm::vm::ClassLoader;
+use rust_jvm::vm::sig;
+use rust_jvm::vm::symref;
+use rust_jvm::vm::value::Value;
+
+#[test]
+fn test_fibonacci() {
+    let mut class_loader = ClassLoader::new();
+    let class = class_loader.resolve_class(&sig::Class::Scalar(String::from("Fibonacci")));
+
+    let sig = sig::Method {
+        name: String::from("fib"),
+        params: vec![sig::Type::Int],
+        return_type: Some(sig::Type::Int),
+    };
+
+    let symref = symref::Method {
+        class: class.symref.clone(),
+        sig: sig,
+    };
+
+    let method = class.find_method(&mut class_loader, &symref);
+    let mut args = vec![];
+    args.push(Value::Int(Wrapping(10)));
+    method.invoke(&class, &mut class_loader, Some(args));
+}
