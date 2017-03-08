d800de948b46f337607850fae8588832d0540cb6
Benjamin Cheng <ben@bcheng.cf>
2017-03-07 23:24:42 -0500

    Generate function signatures

--- src/vm/native.rs
+++ src/vm/native.rs
@@ -1,8 +1,10 @@
-use vm::value::Value;
-use vm::symref;
+use super::symref;
+use super::sig;
+use super::value::Value;
 
 use lib::{Library, Symbol};
 
+use std::ffi::CString;
 use std::io::Result;
 use std::ptr;
 
@@ -12,16 +14,34 @@ pub fn load(path: &str) -> Library {
     Library::new(path).unwrap()
 }
 
+pub fn get_function_signature(symref: &symref::Method) -> CString {
+    let mut signature = String::new();
+    if let sig::Class::Scalar(ref class_sig) = symref.class.sig {
+        for class_segment in class_sig.split("/") {
+            signature.push_str(class_segment);
+            signature.push_str("_");
+        }
+    } else {
+        panic!("Error invoking {:?}, only Scalar classes can have native methods",
+               symref);
+    }
+
+    signature.push_str(symref.sig.name.replace("_", "1_").as_str());
+
+    return CString::new(signature).unwrap();
+}
+
 pub fn has_method(lib: &Library, symref: &symref::Method) -> bool {
-    unsafe { lib.get::<JniFn>(b"java_lang_System_println\0").is_ok() }
+    unsafe { lib.get::<JniFn>(get_function_signature(symref).as_bytes()).is_ok() }
 }
 
 pub fn invoke(lib: &Library,
               symref: &symref::Method,
               args_opt: Option<Vec<Value>>)
               -> Option<Value> {
+
     unsafe {
-        let func: Symbol<JniFn> = lib.get(b"java_lang_System_println\0").unwrap();
+        let func: Symbol<JniFn> = lib.get(get_function_signature(symref).as_bytes()).unwrap();
         if args_opt.is_none() {
             func(0, ptr::null())
         } else {
